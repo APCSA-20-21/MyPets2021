bbd53f471ad1dbc2f3fabbf558273aa1f68ba456
Jeanine Rioux <jrioux@mvusd.net>
2021-02-08 20:21:51 +0000

    Initial commit

---
+++ PetParent.java
@@ -0,0 +1,26 @@
+class PetParent
+{
+  public String name;
+  public boolean hasHair;
+  public String color;
+  public int cuteness;//1-10 1: ugly
+  public double friendliness; //% in decimal form
+
+  public PetParent(String name, boolean hasHair, String color, int cuteness, double friendliness)
+  {
+    this.name = name;
+    this.hasHair = hasHair;
+    this.color = color;
+    this.cuteness = cuteness;
+    this.friendliness = friendliness;
+  }
+
+  public String toString()
+  {
+    return "Name = " + name +
+            "\nhasHair = " + hasHair +
+            "\ncolor = " + color +
+            "\ncuteness (1-10) = " + cuteness +
+            "\nfriendliness (% in decimal form) = " + friendliness;
+  }
+}
